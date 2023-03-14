7b63999185a6cb834f9f3e614e3903cdb25b39ed
Gabriel Machado <gabriel.mach@hotmailcom>
2023-03-14 13:20:58 -0300

    Projeto Hello World

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("Hello World"); 
+System.out.println("\nOlá turma 62!");
+
+	}
+
+}
