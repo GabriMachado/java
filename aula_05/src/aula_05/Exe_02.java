19a97cec7ed895f7760452159c7fe351df9694f0
Gabriel Machado <gabriel.mach@hotmailcom>
2023-03-22 21:59:11 -0300

    Exercicios aula 05

---
+++ aula_05/src/aula_05/Exe_02.java
@@ -0,0 +1,47 @@
+package aula_05;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Exe_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			Scanner leia = new Scanner(System.in);
+			String cor ;
+			ArrayList <String> cores = new ArrayList<String>();
+
+				
+				for(int c=1;c<6;c++) {
+				System.out.println("Digite a "+c+ " cor: ");
+				cor = leia.next();
+				cores.add(cor);
+				}
+				for(int c=1;c<6;c++) {
+					System.out.println("------------------");
+					System.out.println("Lista de cores");
+					System.out.println("------------------");
+					System.out.println(cores);
+					break;
+				}
+				
+				Collections.sort(cores);
+				System.out.println("\nOrdenar cores");
+				for (String cCor : cores) {
+		            System.out.println(cCor);
+				}
+				
+				
+
+			leia.close();
+				
+		
+		
+		
+		
+	}
+
+}
