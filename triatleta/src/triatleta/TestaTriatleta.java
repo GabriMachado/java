7c63bb5993c95b28fc2a7710b033d620de74860c
Gabriel Machado <gabriel.mach@hotmailcom>
2023-03-28 15:32:35 -0300

    Exercicios aula 09

---
+++ triatleta/src/triatleta/TestaTriatleta.java
@@ -0,0 +1,18 @@
+package triatleta;
+
+public class TestaTriatleta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Triatletas tt1 = new Triatletas ("Beatriz");
+		tt1.aquecer();
+		tt1.correr();
+		tt1.nadar();
+		tt1.pedalar();
+		tt1.cansar();
+		
+		
+	}
+
+}
